add462388228ed778f61dee69b95c12aafa5c6c6
Håvard Markhus <haavasma@stud.ntnu.no>
2020-09-25 21:14:20 +0200

    added camera mechanics

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+
+    public float lookXLimit = 60.0f;
+
+    public float speed = 5.0f;
+
+    public float zoomSpeed = 2.0f;
+    private GameObject planets;
+
+    private Transform camera;
+    private Transform selectedPlanet;
+
+    private int selectIndex=0;
+
+    private int childCount;
+
+    private float scrollValue;
+
+    Vector2 rotation = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = playerCameraParent.GetChild(0).transform;
+        planets = GameObject.FindWithTag("Planets");
+        childCount = planets.transform.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            if(Input.GetKeyDown(KeyCode.Q)){
+                selectIndex += childCount  - 1;
+                selectIndex %= childCount;
+            } else if(Input.GetKeyDown(KeyCode.E)){
+                selectIndex+=1;
+                selectIndex%=childCount;
+            }
+            scrollValue += Input.mouseScrollDelta.y;
+
+            planets = GameObject.FindWithTag("Planets");
+            selectedPlanet = planets.transform.GetChild(selectIndex).gameObject.transform;
+            float step =  100f * Time.deltaTime;
+            Debug.Log(selectedPlanet.position);
+            playerCameraParent.position = Vector3.MoveTowards(playerCameraParent.position, selectedPlanet.position, step*speed);
+            camera.localPosition = Vector3.MoveTowards(camera.localPosition, new Vector3(0.0f, 0.0f, selectedPlanet.localScale.x*(3.0f - (scrollValue*0.5f))), 
+                                                            step*zoomSpeed);
+            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+            //playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+            playerCameraParent.eulerAngles = new Vector2(-rotation.x, rotation.y);
+    }
+}
--- Assets/Scripts/PlanetMovement.cs
+++ Assets/Scripts/PlanetMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlanetMovement : MonoBehaviour
 {
     [SerializeField] float radius; 
-
-    [SerializeField] float tilt;
-
     [SerializeField] float torque;
     [SerializeField] float alpha;
     [SerializeField] float orbitSpeed;
-    [SerializeField] Vector3 center;
+    [SerializeField] bool moon;
+    
+    [SerializeField] GameObject centerObject;
+    
+    private Vector3 center;
     Rigidbody rb;
 
     private bool inOrbit = true;
@@ -19,30 +20,34 @@ public class PlanetMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        center = centerObject.transform.position;
         rb = GetComponent<Rigidbody> ();
         transform.position=new Vector3(center.x + radius*Mathf.Cos(alpha), center.y, center.z + radius * Mathf.Sin(alpha));
         alpha+=orbitSpeed*0.01f;
         rb.AddForce(new Vector3(center.x + radius*Mathf.Cos(alpha) - transform.position.x, center.y - transform.position.y, center.z + radius * Mathf.Sin(alpha) - transform.position.z).normalized*400f*orbitSpeed);
-        rb.AddTorque(this.transform.up * torque*100.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        center = centerObject.transform.position;
     }
 
     void FixedUpdate() 
     {
-        Vector3 sunPlanetVector = transform.position - transform.parent.gameObject.transform.position;
-        if(inOrbit){
-            if(sunPlanetVector.magnitude > radius) {
-                rb.AddForce((-sunPlanetVector.normalized) * 10f * orbitSpeed);
+            Vector3 sunPlanetVector = transform.position - center;
+            if(inOrbit){
+                if(moon){
+                    alpha+=orbitSpeed*0.01f;
+                    rb.MovePosition(new Vector3(center.x + radius*Mathf.Cos(alpha), center.y, center.z + radius * Mathf.Sin(alpha)));
+                }else if(sunPlanetVector.magnitude > radius) {
+                    rb.AddForce((-sunPlanetVector.normalized) * 10f * orbitSpeed);
+                }
+                transform.Rotate(0.0f, torque, 0.0f, Space.Self);
+            } else {
+                rb.AddForce((-sunPlanetVector.normalized) * 100f 
+                            * ((centerObject.transform.localScale.x) * (transform.localScale.x))/(sunPlanetVector.magnitude * sunPlanetVector.magnitude));
             }
-        } else {
-            rb.AddForce((-sunPlanetVector.normalized) * 700f 
-                        * ((transform.parent.gameObject.transform.localScale.x) * (transform.localScale.x))/(sunPlanetVector.magnitude * sunPlanetVector.magnitude));
-        }
     }
 
     void OnCollisionEnter(Collision collision)
